d3761c7868e351957f7bd1c8e14e8ffe4ba22c50
asadbekme <asadbekme2002@gmail.com>
2023-07-21 13:26:57 +0500

    background added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Container, Divider, Stack } from "@chakra-ui/react";
+import { Container, Divider, Stack, Center } from "@chakra-ui/react";
 import { NewTodo, TodoViewer } from "./components";
 
 function App() {
   return (
-    <Container mt="5" mx="auto">
-      <Stack spacing={4}>
-        <NewTodo />
-        <TodoViewer />
-        <Divider />
-      </Stack>
+    <Container>
+      <Center h="100vh">
+        <Stack spacing={4}>
+          <NewTodo />
+          <TodoViewer />
+          <Divider />
+        </Stack>
+      </Center>
     </Container>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.tsx";
-// import './index.css';
+import "./index.css";
 
 const client = new QueryClient();
 
